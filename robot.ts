c04e69b0a005b3712eeda2fcfcf0929968389506
Josh Jahans <josh.jahans3@gmail.com>
2024-10-22 00:11:33 +0100

    add scent detection logic

--- robot.ts
+++ robot.ts
@@ -1,11 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
-// Take input:
-// upper-right bounds of world (x, y)
-// initial position (x, y) and initial direction (N, S, E, W)
-// a series of instructions ('L', 'R', 'F')
-
-const inputFile = readFileSync("./input-test.txt", "utf-8");
+const inputFile = readFileSync("./input.txt", "utf-8");
 
 export function validateBounds(bounds: string) {
   if (bounds === "" || !bounds.match(/^\d+ \d+$/)) {
@@ -34,6 +29,14 @@ export function validateInstructionSet(
 
 const directions = ["N", "E", "S", "W"];
 
+export function detectScent(x: number, y: number, scents: number[][]) {
+  return scents.some(([scentX, scentY]) => scentX === x && scentY === y);
+}
+
+export function addScent(x: number, y: number, scents: number[][]) {
+  scents.push([x, y]);
+}
+
 export function rotateLeft(bearing: string) {
   const index = directions.indexOf(bearing);
   return directions[(index + 3) % 4];
@@ -44,28 +47,60 @@ export function rotateRight(bearing: string) {
   return directions[(index + 1) % 4];
 }
 
-function updateCoordinates(boundX: number, boundY: number, isLost: boolean) {
+function updateCoordinates(boundX: number, boundY: number, scents: number[][]) {
   return (x: number, y: number, bearing: string) => {
-    console.log({ x, y, isLost, bearing });
+    let isLost = false;
     switch (bearing) {
       case "N": {
-        if (y + 1 > boundY) isLost = true;
-        else y++;
+        const nextY = y + 1;
+
+        if (nextY > boundY) {
+          if (!detectScent(x, y, scents)) {
+            isLost = true;
+            addScent(x, y, scents);
+          }
+        } else {
+          y++;
+        }
         break;
       }
       case "E": {
-        if (x + 1 > boundX) isLost = true;
-        else x++;
+        const nextX = x + 1;
+
+        if (nextX > boundX) {
+          if (!detectScent(x, y, scents)) {
+            isLost = true;
+            addScent(x, y, scents);
+          }
+        } else {
+          x++;
+        }
         break;
       }
       case "S": {
-        if (y - 1 < 0) isLost = true;
-        else y--;
+        const nextY = y - 1;
+
+        if (nextY < 0) {
+          if (!detectScent(x, y, scents)) {
+            isLost = true;
+            addScent(x, y, scents);
+          }
+        } else {
+          y--;
+        }
         break;
       }
       case "W": {
-        if (x - 1 < 0) isLost = true;
-        else x--;
+        const nextX = x - 1;
+
+        if (nextX < 0) {
+          if (!detectScent(x, y, scents)) {
+            isLost = true;
+            addScent(x, y, scents);
+          }
+        } else {
+          x--;
+        }
         break;
       }
     }
@@ -78,6 +113,7 @@ export function moveRobot(
   bounds: string,
   initialPosition: string,
   movementInstructions: string,
+  scents: number[][],
 ) {
   const [initialX, initialY, initialDirection] = initialPosition.split(" ");
   const [maxX, maxY] = bounds.split(" ");
@@ -87,9 +123,8 @@ export function moveRobot(
   let x = parseInt(initialX);
   let y = parseInt(initialY);
   let bearing = initialDirection;
-  let isLost = false;
 
-  const update = updateCoordinates(boundX, boundY, isLost);
+  const update = updateCoordinates(boundX, boundY, scents);
 
   for (let i = 0; i < movementInstructions.length; i++) {
     const instruction = movementInstructions[i];
@@ -99,11 +134,12 @@ export function moveRobot(
     } else if (instruction === "R") {
       bearing = rotateRight(bearing);
     } else if (instruction === "F") {
-      ({ x, y, isLost } = update(x, y, bearing));
-    }
-
-    if (isLost) {
-      return `${x} ${y} ${bearing} LOST`;
+      const result = update(x, y, bearing);
+      x = result.x;
+      y = result.y;
+      if (result.isLost) {
+        return `${x} ${y} ${bearing} LOST`;
+      }
     }
   }
 
@@ -112,6 +148,7 @@ export function moveRobot(
 
 export function main(input: string) {
   const [bounds, ...instructions] = input.split("\n");
+  const scents: number[][] = [];
   let output = ``;
 
   validateBounds(bounds);
@@ -125,10 +162,11 @@ export function main(input: string) {
 
     validateInstructionSet(initialPosition, movementInstructions);
 
-    output = `${output}\n${moveRobot(bounds, initialPosition, movementInstructions)}`;
+    output = `${output}\n${moveRobot(bounds, initialPosition, movementInstructions, scents)}`;
   }
 
   writeFileSync("./output.txt", output.trim());
+  console.log(output.trim());
 }
 
 main(inputFile);
