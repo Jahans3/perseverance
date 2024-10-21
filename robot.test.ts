c5bcd94760cdf2067b2a8b5222733d0e805b989b
Josh Jahans <josh.jahans3@gmail.com>
2024-10-21 22:14:04 +0100

    add test cases

---
+++ robot.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "@jest/globals";
+
+describe("perseverance", () => {
+  it("should throw an error if the first line of the input is not a set of positive coordinates", () => {});
+  it("should throw an error if the second line of the input is not a set of positive coordinates followed by a bearing (NSEW)", () => {});
+  it("should throw an error if the third line of the input is not a valid series of movement instructions (LRF)", () => {});
+  it("should produce the expected output for a valid instruction set", () => {});
+  it("should report when a robot is lost", () => {});
+});
--- robot.ts
+++ robot.ts
@@ -1,11 +1,19 @@
+import { readFileSync } from "fs";
+
 // Take input:
 // upper-right bounds of world (x, y)
 // initial position (x, y) and initial direction (N, S, E, W)
 // a series of instructions ('L', 'R', 'F')
 
 function main() {
-  const input = process.argv.slice(2);
-  console.log({ input });
+  const [bounds, ...instructions] = readFileSync("./input.txt", "utf-8").split(
+    "\n",
+  );
+  console.log({
+    bounds,
+    instructions,
+    a: instructions.findIndex((x) => x === ""),
+  });
 }
 
 main();
