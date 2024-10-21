a3dcf2faa9e19cf1fab4a77897ad9d46d369f8a5
Josh Jahans <josh.jahans3@gmail.com>
2024-10-21 22:26:52 +0100

    add test case for initial bounds error

--- robot.test.ts
+++ robot.test.ts
@@ -1,7 +1,13 @@
-import { describe, it } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
+import { main } from "./robot";
 
 describe("perseverance", () => {
-  it("should throw an error if the first line of the input is not a set of positive coordinates", () => {});
+  it("should throw an error if the first line of the input is not a set of positive coordinates", () => {
+    expect(() => main("")).toThrow(
+      "The first line of the input must be a set of positive coordinates. E.g. '5 3'",
+    );
+  });
+
   it("should throw an error if the second line of the input is not a set of positive coordinates followed by a bearing (NSEW)", () => {});
   it("should throw an error if the third line of the input is not a valid series of movement instructions (LRF)", () => {});
   it("should produce the expected output for a valid instruction set", () => {});
