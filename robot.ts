01d394efbee613d0fc4efa37885085be175ac19b
Josh Jahans <josh.jahans3@gmail.com>
2024-10-21 22:47:01 +0100

    move validation errors into own functions

--- robot.ts
+++ robot.ts
@@ -7,19 +7,44 @@ import { readFileSync } from "fs";
 
 const inputFile = readFileSync("./input.txt", "utf-8");
 
-export function main(input: string) {
-  const [bounds, ...instructions] = input.split("\n");
-
+export function validateBounds(bounds: string) {
   if (bounds === "" || !bounds.match(/^\d+ \d+$/)) {
     throw new Error(
       "The first line of the input must be a set of positive coordinates. E.g. '5 3'",
     );
   }
+}
 
-  if (!instructions[0].trim().match(/^\d+ \d+ [NSEW]$/)) {
+export function validateInstructionSet(
+  initialPosition: string,
+  movementInstructions: string,
+) {
+  if (!initialPosition.match(/^\d+ \d+ [NSEW]$/)) {
     throw new Error(
       "The second line of the input must be a set of positive coordinates followed by a bearing (NSEW)",
     );
+  }
+
+  if (!movementInstructions.match(/^[LRF]+$/)) {
+    throw new Error(
+      "The third line of the input must be a valid series of movement instructions (LRF)",
+    );
+  }
+}
+
+export function main(input: string) {
+  const [bounds, ...instructions] = input.split("\n");
+
+  validateBounds(bounds);
+
+  for (let i = 0; i < instructions.length; i += 2) {
+    if (instructions[i] === "") i++;
+    if (!instructions[i + 1]) break;
+
+    const initialPosition = instructions[i].trim();
+    const movementInstructions = instructions[i + 1].trim();
+
+    validateInstructionSet(initialPosition, movementInstructions);
   }
 
   console.log({
